18e29ac40a1c7b98703affd1aa72f51e7e247f67
TheAryanAnode <95536151+TheAryanAnode@users.noreply.github.com>
2026-02-06 23:03:38 -0500

    Update config.ts

--- UHACCS/src/utils/config.ts
+++ UHACCS/src/utils/config.ts
@@ -1,4 +1,4 @@
-export const GOOGLE_MAPS_API_KEY = 'AIzaSyBS7JBa_ARayFgYfFmk5s0BHSSzg6aqnFY';
+export const GOOGLE_MAPS_API_KEY = '';
 
 // Get your free Gemini API key at: https://aistudio.google.com/apikey
-export const GEMINI_API_KEY = 'AIzaSyCS5hN_ptn0TidAnaQvq1AG9cOEfZhPan0';
+export const GEMINI_API_KEY = '';
